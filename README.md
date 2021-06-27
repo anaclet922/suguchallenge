20bd4da17fe5474b072863b5179935af7e45efe4
Anaclet Ahishakiye <a.anaclet920@gmail.com>
2021-06-27 18:05:14 +0200

    README

---
+++ README.md
@@ -0,0 +1,3 @@
+* Script (js) fetch data from API placeholder
+* List all 10 users from response
+* Show user's posts whenever **Get User's Posts** button clicked
--- script.js
+++ script.js
@@ -62,7 +62,7 @@ function getUserPosts(user_id, name){
            }
            else if (xmlhttp.status == 400) {
               console.log('There was an error 400');
-           }
+           } 
            else {
                console.log('something else other than 200 was returned');
            }
